290e7eb7790dcfa107af85db8ec2d30a36891b0c
FeusX <feusuaigax@proton.me>
2025-10-25 12:06:18 +0300

    final

--- esp32-radio.ino
+++ esp32-radio.ino
@@ -6,17 +6,12 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define POT_PIN 34
-#define DEAD_ZONE 20
-#define LED_PIN 32
+#define DEAD_ZONE 30
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 uint16_t last_val = 0;
 float current_freq = 0.0;
-float smoothed_signal = 0.0;
-const float smooth_alpha = 0.15;
-const int led_min_rssi = 12;
-const int led_max_rssi = 18;
 
 void setFrequency(float freq_mhz)
 {
@@ -31,22 +26,6 @@ void setFrequency(float freq_mhz)
   Wire.write(0x10); // mute off, no standby
   Wire.write(0x00);
   Wire.endTransmission();
-}
-
-int readSignal()
-{
-  Wire.requestFrom(0x60, 5);
-  int available = Wire.available();
-  if(available >= 5) 
-  {
-    for (uint8_t k = 0; k < 3; k++) Wire.read();
-    uint8_t byte_four = Wire.read();
-    Wire.read();
-    int signal = byte_four & 0x1F;
-    return signal;
-  }
-  Serial.println("Not enough bytes.");
-  return -1; 
 }
 
 void displayWave()
@@ -82,10 +61,6 @@ void setup()
     Serial.println(F("display failed"));
     delay(1000);
 
-  ledcAttach(LED_PIN, 5000, 8);
-  ledcWrite(LED_PIN, 128);
-  delay(2000);
-  ledcWrite(LED_PIN, 0);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
@@ -107,8 +82,6 @@ void setup()
 
   delay(1000);
 
-  smoothed_signal = readSignal();
-
   setFrequency(90.0);
 }
 
@@ -119,25 +92,10 @@ void loop()
   if(abs(pot_val - last_val) > DEAD_ZONE)
   {
     float freq = map(pot_val, 0, 4095, 1080, 875) / 10.0;
-
     setFrequency(freq);
     current_freq = freq;
     last_val = pot_val;
   }
-
-  int raw_signal = readSignal();
-
-  if(raw_signal <= 0 || raw_signal > 31)
-  { raw_signal = (int)round(smoothed_signal); }
-
-  smoothed_signal = smoothed_signal + smooth_alpha * (raw_signal - smoothed_signal);
-
-  float clamped_signal = constrain(smoothed_signal, led_min_rssi, led_max_rssi);
-
-  int duty = map((int)round(clamped_signal), led_min_rssi, led_max_rssi, 0, 255);
-  duty = constrain(duty, 0, 255);
-  float corrected = pow((float)duty / 255.0, 1.0);
-  ledcWrite(LED_PIN, (int)(corrected * 255 + 0.5));
 
   displayWave();
 
